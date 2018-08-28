1ceaea4a8c8d39e2c619d79037769d38f516069b
Jaume Marhuenda <jmarhuenda@hortonworks.com>
2018-08-27 20:34:41 -0700

    BUG-123911 TEZ-3958: Add internal vertex priority information into the tez dag.dot debug information (Jaume Marhuenda via Gopal V)
    (cherry picked from commit 5949b0cef6e9b23c1ac5c75461c027b40cafc91d)
    
    Change-Id: I855f896ec24f621ac1c4c279a6f3ed34619226dc

--- tez-dag/src/main/java/org/apache/tez/dag/app/dag/DAG.java
+++ tez-dag/src/main/java/org/apache/tez/dag/app/dag/DAG.java
@@ -36,6 +36,8 @@ import org.apache.tez.dag.records.TezDAGID;
 import org.apache.tez.dag.records.TezVertexID;
 import org.apache.tez.serviceplugins.api.DagInfo;
 
+import javax.annotation.Nullable;
+
 /**
  * Main interface to interact with the job.
  */
@@ -92,5 +94,12 @@ public interface DAG extends DagInfo {
   StateChangeNotifier getStateChangeNotifier();
 
   org.apache.tez.dag.api.Vertex.VertexExecutionContext getDefaultExecutionContext();
+
+  /**
+   *
+   * @return the DAGScheduler that will schedule
+   * this DAG, null if it doesn't exist
+   */
+  @Nullable DAGScheduler getDAGScheduler();
 
 }
